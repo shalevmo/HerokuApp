54e2f609d0e71f550c75e6989c8cb7f7ec089ee2
shalev <shalevmo@gmail.com>
2015-06-04 15:22:09 +0300

    First phpstorm commit

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ MongoClient.connect("mongodb://dbuser:123456@ds043082.mongolab.com:43082/restaur
     var car = {name:"Shelis", location:"Netivot", Type:"Halavit"};
     var collection = db.collection('restaurants');
     collection.insert(car);
-    dbitems = collection.find().toArray(function(err, items) {});
+    collection.find().toArray(function(err, items) {dbitems = items;});
 
 });
 
